5c97b6479215bc5130079925d14d514f96887574
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-03-15 22:44:48 +0530

    Update: linter.sh

--- linter.sh
+++ linter.sh
@@ -18,15 +18,24 @@ echo "Linting Python Files with 'black'"
 echo ""
 black .
 echo ""
+LAST_COMMIT="$(git log --pretty=format:'%s' -1)"
+LAST_COMMIT_LINK="https://github.com/crazyuploader/Python/commit/${LAST_COMMIT}"
+DATE="$(date +%m/%d/%y)"
 if [[ -z $(git status --porcelain) ]]; then
     echo "${GREEN}Nothing to lint & push${NC}"
 else
     git config user.email "49350241+crazyuploader@users.noreply.github.com"
     git config user.name "crazyuploader"
     git add .
-    git commit -m "Original Commit: $(git log --pretty=format:'%s' -1)" -m "Travis CI -- Auto Linter Date: $(date +%m/%d/%y)"
+    git commit -m "Travis CI -- Auto Linter"       \
+               -m ""                                \
+               -m "Linting Date: ${DATE}"            \
+               -m ""                                  \
+               -m "Original Commit: ${LAST_COMMIT}"    \
+               -m ""                                    \
+               -m "Original Commit Link: ${LAST_COMMIT_LINK}"
     git push https://crazyuploader:"${GITHUB_TOKEN}"@"${GH_REF}" HEAD:linted --force
     echo ""
-    echo -e "${YELLOW}Linted Python code pushed to branch 'linted'\nat https://github.com/crazyuploader/Python/tree/linted${NC}"
+    echo -e "${YELLOW}Linted Python code pushed to branch 'linted' at https://github.com/crazyuploader/Python/tree/linted${NC}"
     echo ""
 fi
